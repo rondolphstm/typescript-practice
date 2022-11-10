59a8d0d1433da4c25299cd1275136ee4f8e73e96
rondolphstm <rondolphtst.m@gmail.com>
2022-11-10 13:45:08 -0500

    simple functions

---
+++ typescript.ts
@@ -0,0 +1,19 @@
+// console.log("Hello Typescript")
+
+let firstName: string = " ronnie"
+let age: number = 21
+let good: boolean = false
+
+// firstName = 4 not valid
+
+let fruit: string[]=[
+    "bananna",
+    "apple"
+]
+
+console.log(`
+    First Name: ${firstName}
+    Your Age: ${age}
+    you have many ${fruit}
+`)
+
