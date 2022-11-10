d5a1150e4666b369a00b3c199ae074142da10236
rondolphstm <rondolphtst.m@gmail.com>
2022-11-10 14:06:59 -0500

    playing wiht typescript enum

--- typescript.ts
+++ typescript.ts
@@ -17,3 +17,27 @@ console.log(`
     you have many ${fruit}
 `)
 
+enum taxForm{
+    standard = "1040",
+    childCredit = "1065",
+    SCorp =  "1121",
+    CCorp = "1120s"
+}
+
+console.log(`
+    you need form ${taxForm.childCredit}
+`)
+
+
+// let fruit: string[]=[
+//     "bananna",
+//     "apple"
+// ]
+
+function getFruit( index: number): string {
+    return fruit[index]
+}
+
+let chooseFruit: string = getFruit(0)
+
+console.log(` you choose an ${getFruit(0)}`)
